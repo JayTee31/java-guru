0c081c6c501f6f4f1dfb29836544ae9cb503b8a4
Tamas Jarvas <jtamas31@gmail.com>
2021-02-20 21:40:19 +0100

    Exercise 1.2

---
+++ ch-01/Exercise12.java
@@ -0,0 +1,9 @@
+public class Exercise12 {
+    public static void main(String[] args) {
+        System.out.println("I Love Java");
+        System.out.println("I Love Java");
+        System.out.println("I Love Java");
+        System.out.println("I Love Java");
+        System.out.println("I Love Java");
+    }
+}
