152ed43ccead90833ebf77d35d4cfb1a262149a5
Tamas Jarvas <jtamas31@gmail.com>
2021-02-20 22:16:23 +0100

    Exercise 1.8

---
+++ ch-01/Exercise18.java
@@ -0,0 +1,6 @@
+public class Exercise18 {
+    public static void main(String[] args) {
+        System.out.println("The perimeter = " + (2 * 6.5 * 3.14159));
+        System.out.println("The area = " + (6.5 * 6.5 * 3.14159));
+    }
+}
