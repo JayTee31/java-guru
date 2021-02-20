04dc4e5cd591a7213b7c16c10113709c8dadf50e
Tamas Jarvas <jtamas31@gmail.com>
2021-02-20 21:36:18 +0100

    Exercise 1.1

---
+++ ch-01/Exercise11.java
@@ -0,0 +1,7 @@
+public class Exercise11 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java!");
+        System.out.println("Learning Java now!");
+        System.out.println("Programming is fun!");
+    }
+}
