147f1150d7d43c0f5d178f17e96c6ff2a1930c07
Tamas Jarvas <jtamas31@gmail.com>
2021-02-20 21:52:35 +0100

    Exercise 1.3

---
+++ ch-01/Exercise13.java
@@ -0,0 +1,8 @@
+public class Exercise13 {
+    public static void main(String[] args) {
+        System.out.println("    J\n"
+            + "J  aaa    v     vaaa\n"
+            + "J  J  aa    v v    a a\n"
+            + "J   aaaa     v     aaaa");
+    }
+}
