6066945545621171de386fa030fa8d809493c0ec
Tamas Jarvas <jtamas31@gmail.com>
2021-02-20 22:49:41 +0100

    Exercise 1.9

---
+++ ch-01/Exercise19.java
@@ -0,0 +1,7 @@
+public class Exercise19 {
+    public static void main(String[] args) {
+        final float height = 5.3f;
+        final float width = 8.6f;
+        System.out.println("Area = " + (height * width) + "\nPerimeter = " + (2 * (height + width)));
+    }
+}
