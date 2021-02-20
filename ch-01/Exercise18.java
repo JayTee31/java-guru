f47a8d9793238ed7a34155516348fd8373a0e51f
Tamas Jarvas <jtamas31@gmail.com>
2021-02-20 22:25:33 +0100

    Exercise 1.8

--- ch-01/Exercise18.java
+++ ch-01/Exercise18.java
@@ -1,6 +1,8 @@
 public class Exercise18 {
     public static void main(String[] args) {
-        System.out.println("The perimeter = " + (2 * 6.5 * 3.14159));
-        System.out.println("The area = " + (6.5 * 6.5 * 3.14159));
+        final double radius = 6.5;
+        final double pi = 3.14159;
+        System.out.println("The perimeter = " + (2 * radius * pi));
+        System.out.println("The area = " + (radius * radius * pi));
     }
 }
